1df5d2caedcfa279f6494d185702b8ec5fdad3d0
yamadaarch <tutuasublo@gmail.com>
2022-09-12 11:32:31 +0900

    災害を選べるようにした

--- Server/SimpleServer.py
+++ Server/SimpleServer.py
@@ -1,3 +1,4 @@
+from cmath import isnan
 from flask import Flask,render_template,jsonify,make_response,abort,request,url_for
 import peewee
 import json
@@ -35,6 +36,33 @@ class DataModel(peewee.Model):
 # テーブルの作成
 db.create_tables([DataModel])
 
+#災害のリスト
+disaster_list = ["flood","landslide","storm_surge","earthquake","tsunami","fire","inland_flood","volcano"]
+
+#災害を指定して格納する関数
+def disaster_select(datalist,num):
+    if num == 0:
+        datalist = datalist.select().where(DataModel.flood == 1)
+    elif num == 1:
+        datalist = datalist.select().where(DataModel.landslide == 1)
+    elif num == 2:
+        datalist = datalist.select().where(DataModel.storm_surge == 1)
+    elif num == 3:
+        datalist = datalist.select().where(DataModel.earthquake == 1)
+    elif num == 4:
+        datalist = datalist.select().where(DataModel.tsunami == 1)       
+    elif num == 5:
+        datalist = datalist.select().where(DataModel.fire == 1)
+    elif num == 6:
+        datalist = datalist.select().where(DataModel.inland_flood == 1)
+    else:
+        datalist = datalist.select().where(DataModel.volcano == 1)
+    return datalist
+
+
+
+
+
 #現在位置を所得する関数
 def get_locate():
     geo_request_url = 'https://get.geojs.io/v1/ip/geo.json'
@@ -43,33 +71,6 @@ def get_locate():
     keido = geo_data['longitude']
     return ido,keido
 
-# API実装
-# データ取得API→Chart.jsで参照するのに使う
-@app.route('/getData/', methods=['GET'])
-def get_Ventilations():
-    print("dooooo_getdata")
-    #データベースからデータを取ってくる
-    #都市順に並べ替え
-    datalist = DataModel.select().order_by(DataModel.city)
-    # グラフ描画用データセットを準備する。
-    labels = []
-    dataset = {
-            'data':[]
-            }
-    # データを読み込んで、グラフ用に編集しながら追加していく。
-    #ラベルに都市名、データに今の値
-    for v in datalist:
-        labels.append(v.city)
-        dataset['data'].append(v.now)
-
-    # JSON形式で戻り値を返すために整形
-    result = {
-            "labels":labels,
-            "datasets":[dataset]}
-    print(result)
-    #jsonファイルを送るjsonify
-    #make_responseで返す
-    return make_response(jsonify(result))
 
 #避難所の絞り込み
 @app.route('/select',methods=['POST'])
@@ -77,7 +78,32 @@ def select():
     ido = request.form.get('緯度')
     keido = request.form.get('経度')
     now_Station = (ido, keido)
+    dis_list = []
+    #　災害の指定されたものをリストに保存：後で関数にする
+    if request.form.get('a') != None:
+        dis_list.append(int(request.form.get('a')))
+    if request.form.get('b') != None:
+        dis_list.append(int(request.form.get('b')))
+    if request.form.get('c') != None:
+        dis_list.append(int(request.form.get('c')))
+    if request.form.get('d') != None:
+        dis_list.append(int(request.form.get('d')))
+    if request.form.get('e') != None:
+        dis_list.append(int(request.form.get('e')))
+    if request.form.get('f') != None:
+        dis_list.append(int(request.form.get('f')))
+    if request.form.get('g') != None:
+        dis_list.append(int(request.form.get('g')))
+    if request.form.get('h') != None:
+        dis_list.append(int(request.form.get('h')))
+    if request.form.get('i') != None:
+        dis_list.append(int(request.form.get('i')))
+
     datalist = DataModel
+    # 災害から避難所絞り込み　
+    for _ in range(len(dis_list)):
+        d = dis_list.pop()
+        datalist = disaster_select(datalist,d)
     result = []
     N = 0.01
     limit = 10 #探す件数
@@ -104,9 +130,6 @@ def select():
     #上位10個に
     result = result[0:limit]
     return render_template('result.html',result=result)
-
-
-
 
 
 # 登録API POSTのみ受付
@@ -139,7 +162,6 @@ def addData():
 
     # データを保存
     v.save()
-    print("doooo_adddata")
     print(v)
     return "ok"
 
